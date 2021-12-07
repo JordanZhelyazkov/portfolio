3da700255f3943ab9d7befe1d83dd88b3c58d4e6
JordanZhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-07 13:39:57 +0200

    optimizing the code and adding 2 functions for better readability

--- scripts.js
+++ scripts.js
@@ -1,45 +1,51 @@
-console.log('something')
+console.log('something');
 
 const slideContainer = document.querySelector('.images-slider');
-const slideImages = document.querySelectorAll('.images-slider img');
-
-
+const slideImages = slideContainer.querySelectorAll('img');
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
-
 let counter = 1;
-
 const size = slideImages[0].clientWidth;
 
-slideContainer.style.transform = "translateX(" + (-size * counter) + "px)";
+setTransform(slideContainer, counter);
 
 prevBtn.addEventListener("click", () => {
-    if(counter <= 0) { return }
-    slideContainer.style.transition = "0.5s ease-in-out";
+    if(counter <= 0) {
+        return;
+    }
+      setTransition(slideContainer, counter, slideImages);
     counter--;
-    slideContainer.style.transform = "translateX(" + (-size * counter) + "px)";
+      setTransform(slideContainer, counter);
 })
 
 nextBtn.addEventListener("click", () => {
     if(counter >= slideImages.length - 1) { return }
-    slideContainer.style.transition = "0.5s ease-in-out";
+      setTransition(slideContainer, counter, slideImages);
     counter++;
-    slideContainer.style.transform = "translateX(" + (-size * counter) + "px)";
+      setTransform(slideContainer, counter);
 })
 
 slideContainer.addEventListener("transitionend", () => {
-
-    if(slideImages[counter].id === 'last-img'){
-        slideContainer.style.transition = 'none';
+    setTransition(slideContainer, counter, slideImages);
+    if(slideImages[counter].id === 'last-img') {
         counter = slideImages.length -2;
-        slideContainer.style.transform = "translateX(" + (-size * counter) + "px)";
-
     }
-
-    if(slideImages[counter].id === 'first-img'){
-        slideContainer.style.transition = 'none';
+    if(slideImages[counter].id === 'first-img') {
         counter = slideImages.length - counter;
-        slideContainer.style.transform = "translateX(" + (-size * counter) + "px)";
-
     }
+      setTransform(slideContainer, counter);
 })
+
+function setTransition(target, count, picturesContainer) {
+    if(picturesContainer[count].id === 'first-img' || picturesContainer[count].id === 'last-img') {
+        target.style.transition = "none";
+    } else {
+        target.style.transition = "0.5s ease-in-out";
+    }
+        return target;
+}
+
+function setTransform(target, count) {
+    target.style.transform = "translateX(" + (-size * count) + "px)";
+        return target;
+}
