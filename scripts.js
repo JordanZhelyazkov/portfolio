e95c73d32bf6f05699e3b221178cd79506b49bad
JordanZhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-08 15:43:47 +0200

    removing the minutes from the countdown, because it was ugly and putting : between

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ console.log('something')
 const display = document.querySelector(".display");
 const subDisplay = document.querySelector(".sub-display");
 const subText = document.querySelector(".sub-display")
-subDisplay.innerText = "DAYS  /  HOURS  /  MINUTES";
+subDisplay.innerText = "DAYS : HOURS";
 
 let timer = setInterval(function() {
         const countDownDate = new Date("Jan 1, 2022 00:00:00");
@@ -13,8 +13,7 @@ let timer = setInterval(function() {
         }
         let days = Math.floor(distanceBetween / (1000 * 60 * 60 * 24));
         let hours = Math.floor((distanceBetween / (1000 * 60 * 60 * 24)) * 24);
-        let minutes = Math.floor((distanceBetween / (1000 * 60 * 60 * 24)) * 24 * 60);
-        let result = `${days} / ${hours} / ${minutes}`;
+        let result = `${days}:${hours}`;
 
         if(distanceBetween < 0){
             clearInterval(timer);
