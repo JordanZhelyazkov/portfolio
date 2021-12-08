da596ce098851c26eea7d3e5b1aad7cf8c64876e
JordanZhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-08 14:20:55 +0200

    creating countdown timer to new year and styling it

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
 console.log('something')
+const display = document.querySelector(".display");
+const subDisplay = document.querySelector(".sub-display");
+const subText = document.querySelector(".sub-display")
+subDisplay.innerText = "DAYS  /  HOURS  /  MINUTES";
 
+let timer = setInterval(function() {
+        const countDownDate = new Date("Jan 1, 2022 00:00:00");
+        let currentTime = new Date();
+        let distanceBetween = countDownDate.getTime() - currentTime.getTime();
+        if (distanceBetween === 0) {
+            alert('Happy New Year');
+        }
+        let days = Math.floor(distanceBetween / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distanceBetween / (1000 * 60 * 60 * 24)) * 24);
+        let minutes = Math.floor((distanceBetween / (1000 * 60 * 60 * 24)) * 24 * 60);
+        let result = `${days} / ${hours} / ${minutes}`;
 
-
-  
+        if(distanceBetween < 0){
+            clearInterval(timer);
+        }
+            return display.innerHTML = result;
+        }, 1000 );
