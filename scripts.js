2b8023d6531bade1a03b3db98dd317aa5ee24af0
JordanZhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-09 10:36:04 +0200

    removing interval clear function and making the timer to work infinitely and alerting when new year comes

--- scripts.js
+++ scripts.js
@@ -1,22 +1,37 @@
-console.log('something')
+console.log('something');
 const display = document.querySelector(".display");
 const subDisplay = document.querySelector(".sub-display");
-const subText = document.querySelector(".sub-display")
-subDisplay.innerText = "DAYS : HOURS";
+subDisplay.innerText = "DAYS:HOURS:MINUTES:SECONDS";
 
-let timer = setInterval(function() {
-        const countDownDate = new Date("Jan 1, 2022 00:00:00");
+   let timer = setInterval(function() {
+        let nextYear = new Date().getFullYear() + 1;
+        let countDownDate = new Date(`Jan 1, ${nextYear} 00:00:00`);
         let currentTime = new Date();
         let distanceBetween = countDownDate.getTime() - currentTime.getTime();
-        if (distanceBetween === 0) {
-            alert('Happy New Year');
-        }
         let days = Math.floor(distanceBetween / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((distanceBetween / (1000 * 60 * 60 * 24)) * 24);
-        let result = `${days}:${hours}`;
-
-        if(distanceBetween < 0){
-            clearInterval(timer);
-        }
+        let getTheRest = getHoursAndSeconds(days);
+        let result = `${days} ${getTheRest}`;
             return display.innerHTML = result;
         }, 1000 );
+
+
+   function getHoursAndSeconds(days) {
+        const today = new Date();
+        let hours = today.getHours();
+        let minutes = today.getMinutes();
+        let seconds = today.getSeconds();
+        hours = (24 - hours) - 1;
+        minutes = (60 - minutes) - 1;
+        seconds = (60 - seconds) - 1;
+           if(days === 0 && hours === 0 && minutes === 0 && seconds === 0){
+               alert('Happy New Year')
+           }
+            return `: ${smallerThanTen(hours)} : ${smallerThanTen(minutes)} : ${smallerThanTen(seconds)}`;
+   }
+
+   function smallerThanTen(num) {
+       if(num < 10){
+           num = "0" + num;
+       }
+       return num
+   }
